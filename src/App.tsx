228b189dd19ddfabfb0057908b4f321406a46080
eduuk15 <98836125+eduuk15@users.noreply.github.com>
2022-11-21 13:48:39 -0300

    feat: adiciona frontend no github

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { GlobalStyles } from './styles/GlobalStyles';
+
+import { Header } from './components/Header';
+
+export function App() {
+
+	return (
+		<>
+			<GlobalStyles />
+			<Header />
+		</>
+	);
+}
