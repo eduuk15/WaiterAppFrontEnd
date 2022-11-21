725685c5a5fcc4ec94c8afb93dad76442127baac
eduuk15 <98836125+eduuk15@users.noreply.github.com>
2022-11-21 14:15:50 -0300

    feat: adiciona estilizacao para header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,20 @@
 
-import logo from '../assets/images/logo.svg';
+import logo from '../../assets/images/logo.svg';
 
 import { Container } from './styles';
+import { Content } from './styles';
 
 export function Header() {
 	return (
 		<Container>
-			<div className="page-details">
-				<h1>Pedidos</h1>
-				<h2>Acompanhe os pedidos dos clientes</h2>
-			</div>
+			<Content>
+				<div className="page-details">
+					<h1>Pedidos</h1>
+					<h2>Acompanhe os pedidos dos clientes</h2>
+				</div>
 
-			<img src={logo} alt="WAITERAPP" />
+				<img src={logo} alt="WAITERAPP" />
+			</Content>
 		</Container>
 	);
 }
