6a5c60084f689017baeef24841d11aa37783e338
eduuk15 <98836125+eduuk15@users.noreply.github.com>
2022-11-21 15:38:20 -0300

    feat: adicona pros dinamicas

--- src/components/Orders/index.tsx
+++ src/components/Orders/index.tsx
@@ -4,9 +4,18 @@ import { Container } from './styles';
 export function Orders() {
 	return (
 		<Container>
-			<OrdersBoard />
-			<OrdersBoard />
-			<OrdersBoard />
+			<OrdersBoard
+				icon="⌛"
+				title="Fila de espera"
+			/>
+			<OrdersBoard
+				icon="🧑‍🍳"
+				title="Em preparação"
+			/>
+			<OrdersBoard
+				icon="✅"
+				title="Pronto!"
+			/>
 		</Container>
 	);
 }
