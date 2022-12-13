665317c5223d6db5cf34829d5f01437d7889b200
eduuk15 <98836125+eduuk15@users.noreply.github.com>
2022-12-13 18:46:45 -0300

    feat: renderiza titulo e icone de botao conforme status atualizado

--- src/components/OrderModal/index.tsx
+++ src/components/OrderModal/index.tsx
@@ -11,9 +11,10 @@ interface OrderModalProps {
 	onClose: () => void;
 	onCancelOrder: () => Promise<void>;
 	isLoading: boolean;
+	onChangeOrderStatus: () => void;
 }
 
-export function OrderModal({ visible, order, onClose, onCancelOrder, isLoading }: OrderModalProps) {
+export function OrderModal({ visible, order, onClose, onCancelOrder, isLoading, onChangeOrderStatus }: OrderModalProps) {
 	useEffect(() => {
 		function handleKeyDown(event: KeyboardEvent) {
 			if (event.key == 'Escape') {
@@ -91,10 +92,18 @@ export function OrderModal({ visible, order, onClose, onCancelOrder, isLoading }
 				</OrderDetails>
 
 				<Actions>
-					<button type="button" className="primary" disabled={isLoading}>
-						<span>🧑‍🍳</span>
-						<strong>Iniciar produçao</strong>
-					</button>
+					{order.status !== 'DONE' && (
+						<button type="button" className="primary" disabled={isLoading} onClick={onChangeOrderStatus}>
+							<span>
+								{order.status === 'WAITING' && '🧑‍🍳'}
+								{order.status === 'IN_PRODUCTION' && '✅'}
+							</span>
+							<strong>
+								{order.status === 'WAITING' && 'Iniciar Produção'}
+								{order.status === 'IN_PRODUCTION' && 'Concluir Pedido'}
+							</strong>
+						</button>
+					)}
 
 					<button
 						type="button"
--- src/components/OrdersBoard/index.tsx
+++ src/components/OrdersBoard/index.tsx
@@ -10,9 +10,10 @@ interface OrdersBoardProps {
 	title: string;
 	orders: Order[];
 	onCancelOrder: (orderId: string) => void;
+	onChangeOrderStatus: (orderId: string, status: Order['status']) => void;
 }
 
-export function OrdersBoard({ icon, title, orders, onCancelOrder }: OrdersBoardProps) {
+export function OrdersBoard({ icon, title, orders, onCancelOrder, onChangeOrderStatus }: OrdersBoardProps) {
 	const [isModalVisible, setIsModalVisible] = useState(false);
 	const [selectedOrder, setSelectedOrder] = useState<null | Order>(null);
 	const [isLoading, setIsLoading] = useState(false);
@@ -25,6 +26,20 @@ export function OrdersBoard({ icon, title, orders, onCancelOrder }: OrdersBoardP
 	function handleCloseModal() {
 		setIsModalVisible(false);
 		setSelectedOrder(null);
+	}
+
+	async function handleChangeOrderStatus() {
+		setIsLoading(true);
+
+		const status = selectedOrder?.status === 'WAITING' ? 'IN_PRODUCTION' : 'DONE';
+
+		await api.patch(`/orders/${selectedOrder?._id}`, { status });
+
+		toast.success(`O pedido da mesa ${selectedOrder?.table} teve o status alterado!`);
+
+		onChangeOrderStatus(selectedOrder!._id, status);
+		setIsLoading(false);
+		setIsModalVisible(false);
 	}
 
 	async function handleCancelOrder() {
@@ -47,6 +62,7 @@ export function OrdersBoard({ icon, title, orders, onCancelOrder }: OrdersBoardP
 				onClose={handleCloseModal}
 				onCancelOrder={handleCancelOrder}
 				isLoading={isLoading}
+				onChangeOrderStatus={handleChangeOrderStatus}
 			/>
 			<header>
 				<span>{icon}</span>
